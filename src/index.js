bd6e1a00ea69ed8fc02505b899d027b8f37d66d8
drbarq <J.Tustin@gmail.com>
2019-10-17 13:30:41 -0600

    addin in store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-// import { Provider } from 'react-redux'; /* code change */
+import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(
-    shoppingListItemReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && Window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+// const store = createStore(
+//     shoppingListItemReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     // window.__REDUX_DEVTOOLS_EXTENSION__ && Window.__REDUX_DEVTOOLS_EXTENSION__()
+//     );
+
+const store = createStore(shoppingListItemReducer)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    document.getElementById('root')
+)   
+
 
 // ReactDOM.render(
 //     <Provider store={store}>
@@ -21,4 +32,4 @@ const store = createStore(
 //     document.getElementById('root')
 //     );
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'))
+// ReactDOM.render(<App store={store}/>, document.getElementById('root'))
