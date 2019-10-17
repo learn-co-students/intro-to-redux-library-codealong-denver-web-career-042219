ce364e919f6fe70528f72fa3d598f04bb01f2306
drbarq <J.Tustin@gmail.com>
2019-10-17 13:44:12 -0600

    addin in reducer

--- src/index.js
+++ src/index.js
@@ -14,13 +14,16 @@ import './index.css';
 //     // window.__REDUX_DEVTOOLS_EXTENSION__ && Window.__REDUX_DEVTOOLS_EXTENSION__()
 //     );
 
-const store = createStore(shoppingListItemReducer)
+const store = createStore(
+        shoppingListItemReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>
+    </Provider>,
     document.getElementById('root')
 )   
 
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -7,14 +7,34 @@ export default function shoppingListItemReducer(
 	console.log(action)
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			console.log('Current state.items length %s', state.items.length);
-			console.log('Updating state.items length to %s', state.items.length + 1)
-			return Object.assign({}, state, {
+			console.log('Current state.items.length', state.items.length)
+			console.log('Current state.items.length updating', state.items.length + 1)
+			return {
+				...state,
 				items: state.items.concat(state.items.length + 1)
-			})
+			}
 
 		default:
-			console.log('Initial state.items lenght: %s', state.items.length)
-			return state;
+			console.log('Initial state.items length', state.items.length)
+			return state
 	}
+
+
+
+
 }
+
+// 	console.log(action)
+// 	switch (action.type) {
+// 		case 'INCREASE_COUNT':
+// 			console.log('Current state.items length %s', state.items.length);
+// 			console.log('Updating state.items length to %s', state.items.length + 1)
+// 			return Object.assign({}, state, {
+// 				items: state.items.concat(state.items.length + 1)
+// 			})
+
+// 		default:
+// 			console.log('Initial state.items lenght: %s', state.items.length)
+// 			return state;
+// 	}
+// }
